eaf965a0a2bae452065678d2cf79372dff673747
Maksymilian <s25940@pjwstk.edu.pl>
2024-06-17 15:05:38 +0200

    chore: updated style for chat list and added temporary image files for testing

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,20 +1,74 @@
-import { useState } from 'react';
-import './chat-list.css'
+import { useState } from "react";
+import "./chat-list.css";
 
 const ChatList = () => {
-    const [addMode, setAddMode] = useState(false);
+  const [addMode, setAddMode] = useState(false);
 
-    return (
-        <div className="chat-list">
-            <div className="search">
-                <div className="search-bar">
-                    <img src="./search.png" alt="search" />
-                    <input type="text" placeholder="Search" />
-                </div>
-                <img src="./add.png" alt="add-chat" />
-            </div>
+  return (
+    <div className="chat-list">
+      <div className="search">
+        <div className="search-bar">
+          <img className="search" src="./search.png" alt="search" />
+          <input type="text" placeholder="Search" />
         </div>
-    )
-}
+        <img
+          className="add"
+          src={addMode ? "./remove.png" : "./add.png"}
+          alt="add-chat"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.jpg" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.jpg" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.jpg" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.jpg" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.jpg" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.jpg" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.jpg" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ChatList;
